480fa8d3663b10be62172c59c799f263a5802875
NotoriousPSV <pshovad@gmail.com>
2025-06-25 17:48:58 -0500

    The premptive tutorial portion for the user to understand the game before playing.

--- Currency-Exchange-Withatwist.py
+++ Currency-Exchange-Withatwist.py
@@ -5,3 +5,15 @@ This project is to primarily speed up my learning for python, and I will be usin
 
 So, yeah!
 '''
+#Introduction
+print("Hello! You are the newest employee in the currency exchange department. Yay! You will be sooo good, I can already feel it!")
+print("\nLet me give you the rundown on this place, your main job is to help people exchange their currency, but you can get into a little forex trading if you want.") 
+print("\nJust don't trade using insider info, that's a big no no.")
+print("\nDepending on different events, the price of the currency will either rise or fall.") 
+print("\nYou want to buy low and sell high if you go long, and sell high and buy low if you short.")
+print("\nCertain clients may ask for advice along the way, though they should likely be seeking advice from others.") 
+print("\nNevertheless, it's in your best interest to give them good advice, because you get 10 percent of their profits from their trade. ")
+print("\nAt times, certain goverment officials may come towards you. Don't be scared! They can on slim occasions be nice. ")
+print("\nYour goal is to keep a profit, if you have too much debt, you could be thrown in jail for tax evasion. ")
+print("\nThat concludes the tutorial! If you have any questions, go to the menu and find the FAQs area to see if your queries can be answered. ")
+
