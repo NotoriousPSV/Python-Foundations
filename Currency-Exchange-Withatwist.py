da0b58d54f9671adac8d450940e62eb83689bc02
NotoriousPSV <pshovad@gmail.com>
2025-06-25 16:52:12 -0500

    Project Description Comment

---
+++ Currency-Exchange-Withatwist.py
@@ -0,0 +1,7 @@
+'''
+This project will be a currency converter, with a twist! It will be a semi-story game, with aspects of currency exchange. 
+
+This project is to primarily speed up my learning for python, and I will be using functions. Yay!!
+
+So, yeah!
+'''
